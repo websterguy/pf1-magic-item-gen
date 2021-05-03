8432d1e536aab896a3c92f2c3cceca569e475237
websterguy <54455090+websterguy@users.noreply.github.com>
2021-05-03 04:58:44 -0700

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,14 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on("renderActorDirectory", (app, html, data) => {
+    //sbcConfig.options.debug && sbcUtils.log("Rendering sbc button")  
+    const createMagicItemButton = $("<button id='createMagicItemButton' class='create-entity sbcButton'><i class='fas fa-file-import'></i></i>Create Magic Item</button>");
+    html.find(".directory-footer").append(createMagicItemButton)
+    createMagicItemButton.click(async (ev) => {
+        //await sbcApp.initializeSBC()
+        //sbcApp.startSBC()
+    });
+    
+});
