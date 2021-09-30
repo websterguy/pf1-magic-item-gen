5b334a8daabb2a8ad8ebfaeab8323311dcc4d379
websterguy <54455090+websterguy@users.noreply.github.com>
2021-09-29 20:49:37 -0700

    add macro hook call

--- scripts/itemGen.js
+++ scripts/itemGen.js
@@ -7,11 +7,17 @@ export class itemGen {
     }
 }
 
-    Hooks.on("renderItemDirectory", (app, html, data) => {
-        const startGenButton = $("<button id='startItemGenButton' class='create-entity itemGenButton'><i class='fas fa-shield-alt'></i></i>Create Magic Item</button>");
-        html.find(".directory-footer").append(startGenButton);
-        startGenButton.click(async (ev) => {
-            itemGen.startItemGen();
-        });
-
+Hooks.on("renderItemDirectory", (app, html, data) => {
+    const startGenButton = $("<button id='startItemGenButton' class='create-entity itemGenButton'><i class='fas fa-shield-alt'></i>Create Magic Item</button>");
+    html.find(".directory-footer").append(startGenButton);
+    startGenButton.click(async (ev) => {
+        itemGen.startItemGen();
     });
+
+});
+
+Hooks.once("init", () => {
+    game.magicItemGen = game.magicItemGen || {};
+
+    game.magicItemGen.itemGen = itemGen.startItemGen;
+})
